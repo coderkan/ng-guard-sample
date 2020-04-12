10fa7c319a4b916924b56cc20f31e5f4672d18f6
coderkan <erkanguzeler@gmail.com>
2020-04-12 21:07:50 +0300

    [IMPROVEMENT] admin and user dash board ui finished.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ng-guard';
+
+  @ViewChild('closeModal') closeModal: ElementRef;
+
+  isLoggedIn = false;
+
+  constructor(private router: Router) {
+    this.isLoggedIn = false;
+  }
+  ngOnInit(): void {
+  }
+
+
+  loginAsUser() {
+    this.isLoggedIn = true;
+  }
+
+  loginAsAdmin() {
+    console.log('Login As aDmin');
+    this.isLoggedIn = true;
+    this.closeModal.nativeElement.click();
+
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.router.navigate(['/home']);
+  }
+
+  goToDashBoard() {
+    this.router.navigate(['/admin']);
+  }
+
 }
